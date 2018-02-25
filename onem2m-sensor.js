172b77be20750a6c380beceef07ac93a3ff05504
Mahdi Ben Alaya <benalaya.mahdi@gmail.com>
2018-02-25 21:37:13 +0100

    Update onem2m-sensor.js

--- onem2m-sensor.js
+++ onem2m-sensor.js
@@ -1,47 +1,25 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var request = require('request');
-var app = express();
 
+createAE();
+function createAE(){
+	console.log("\n▶▶▶▶▶");
+	var originator = "Cae-sensor";
+	var method = "POST";
+	var uri= "http://127.0.0.1:8080/~/server/server";
+	var resourceType=2;
+	var requestId = "123456";
+	var representation = {
+		"m2m:ae":{
+			"rn":"mysensor",			
+			"api":"app.company.com",
+			"rr":"false"
+		}
+	};
 
-var aeJson = {
-	"m2m:ae":{
-		"rn":"mysensor",			
-		"api":"app.company.com",
-		"rr":"true",
-		"poa":["http://127.0.0.1:3000/"]
-	}
-};
+	console.log(method+" "+uri);
+	console.log(representation);
 
-setTimeout(function() {
-	send("POST","http://127.0.0.1:8080/~/server/server","Cae-sensor","123456","2",aeJson);
-}, 1000)
-
-var cntJson = {
-	"m2m:cnt":{
-		"rn":"light",
-		"mni":100		
-
-	}
-};
-
-setTimeout(function() {
-  send("POST","http://127.0.0.1:8080/~/server/server/mysensor","Cae-sensor","123456","3",cntJson);
-}, 2000)
-
-
-var cinJson = {
-	"m2m:cin":{
-		"con": Math.floor( Math.random() * 10)
-	}
-};
-
-setInterval(function() {
-  send("POST","http://127.0.0.1:8080/~/server/server/mysensor/light","Cae-sensor","123456","4",cinJson);
-}, 3000)
-
-function send(method, uri, originator,requestId, resourceType, representation){
-	console.log("\n"+method+" "+uri);
 	var options = {
 		uri: uri,
 		method: method,
@@ -54,7 +32,90 @@ function send(method, uri, originator,requestId, resourceType, representation){
 	};
 
 	request(options, function (error, response, body) {
-		
+		console.log("◀◀◀◀◀");
+		if(error){
+			console.log(error);
+		}else{
+			console.log(response.statusCode);
+			console.log(body);
+			createContainer();
+		}
+	});
+}
+
+
+function createContainer(){
+	console.log("\n▶▶▶▶▶");
+	var originator = "Cae-sensor";
+	var method = "POST";
+	var uri= "http://127.0.0.1:8080/~/server/server/mysensor";
+	var resourceType=3;
+	var requestId = "123456";
+	var representation = {
+		"m2m:cnt":{
+			"rn":"luminosity",
+			"mni":100		
+
+		}
+	};
+
+	console.log(method+" "+uri);
+	console.log(representation);
+
+	var options = {
+		uri: uri,
+		method: method,
+		headers: {
+			"X-M2M-Origin": originator,
+			"X-M2M-RI": requestId,
+			"Content-Type": "application/json;ty="+resourceType
+		},
+		json: representation
+	};
+
+	request(options, function (error, response, body) {
+		console.log("◀◀◀◀◀");
+		if(error){
+			console.log(error);
+		}else{
+			console.log(response.statusCode);
+			console.log(body);
+			setInterval(function() {
+				createContentInstance();
+			}, 5000);
+		}
+	});
+}
+
+function createContentInstance(){
+	console.log("\n▶▶▶▶▶");
+	var originator = "Cae-sensor";
+	var method = "POST";
+	var uri= "http://127.0.0.1:8080/~/server/server/mysensor/luminosity";
+	var resourceType=4;
+	var requestId = "123456";
+	var representation = {
+		"m2m:cin":{
+			"con": Math.floor(Math.random()*10)
+		}
+	};
+
+	console.log(method+" "+uri);
+	console.log(representation);
+
+	var options = {
+		uri: uri,
+		method: method,
+		headers: {
+			"X-M2M-Origin": originator,
+			"X-M2M-RI": requestId,
+			"Content-Type": "application/json;ty="+resourceType
+		},
+		json: representation
+	};
+
+	request(options, function (error, response, body) {
+		console.log("◀◀◀◀◀");
 		if(error){
 			console.log(error);
 		}else{
@@ -63,5 +124,3 @@ function send(method, uri, originator,requestId, resourceType, representation){
 		}
 	});
 }
-
-
